b4bb4a730bda0237f0b7f1125cff4e192b4130c9
usabarashi <19676305+usabarashi@users.noreply.github.com>
2026-05-03 03:07:09 +0900

    Refactor terminal configuration for clarity and consistency

--- modules/shared/terminal.nix
+++ modules/shared/terminal.nix
@@ -10,47 +10,49 @@
     tmux
   ];
 
-  programs.direnv = {
-    enable = true;
-    nix-direnv.enable = true;
-  };
+  programs = {
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
 
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-  };
+    fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
 
-  programs.zsh = {
-    enable = true;
-    dotDir = "${config.xdg.configHome}/zsh";
+    zsh = {
+      enable = true;
+      dotDir = "${config.xdg.configHome}/zsh";
 
-    envExtra = ''
-      # Nix
-      if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
-        . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
-      fi
-      # End Nix
+      envExtra = ''
+        # Nix
+        if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+          . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+        fi
+        # End Nix
 
-      # Add user tools to PATH
-      export PATH=$HOME/bin:$PATH
-    '';
+        # Add user tools to PATH
+        export PATH=$HOME/bin:$PATH
+      '';
 
-    initContent = ''
-      git-find() {
-        local selected
-        selected=$(ghq list | fzf --query "$*") || return 1
-        cd "$(ghq root)/$selected"
-      }
+      initContent = ''
+        git-find() {
+          local selected
+          selected=$(ghq list | fzf --query "$*") || return 1
+          cd "$(ghq root)/$selected"
+        }
 
-      git-find-widget() {
-        BUFFER="git-find ''${(q)LBUFFER}"
-        zle accept-line
-      }
-      zle -N git-find-widget
-      bindkey '^G' git-find-widget
-    '';
+        git-find-widget() {
+          BUFFER="git-find ''${(q)LBUFFER}"
+          zle accept-line
+        }
+        zle -N git-find-widget
+        bindkey '^G' git-find-widget
+      '';
 
-    syntaxHighlighting.enable = true;
+      syntaxHighlighting.enable = true;
+    };
   };
 
   home.file = {
